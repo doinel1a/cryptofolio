43756fbd75502adb4376fa5c2a6b280178939e55
doinel1a <doinel1atanasiu@gmail.com>
2023-12-10 18:50:33 +0100

    fix: active dark icon to moon start

--- src/components/ui/theme/toggle.tsx
+++ src/components/ui/theme/toggle.tsx
@@ -2,7 +2,7 @@
 
 import * as React from 'react';
 
-import { Laptop, Moon, MoonStar, Sun } from 'lucide-react';
+import { Laptop, MoonStar, Sun } from 'lucide-react';
 import { useTheme } from 'next-themes';
 
 import { Button } from '@/components/ui/button';
@@ -21,7 +21,7 @@ export function ThemeToggle() {
       <DropdownMenuTrigger asChild>
         <Button variant='outline' size='icon'>
           <Sun className='h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
-          <Moon className='absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
+          <MoonStar className='absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
           <span className='sr-only'>Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
